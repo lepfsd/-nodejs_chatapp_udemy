ca349f748214d70eafe39aa426315330637e443f
lepfsd-strapp <lperez@master.inmobilia.com>
2019-10-15 23:11:23 -0400

    socketio

--- server.js
+++ server.js
@@ -1,28 +1,36 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+var express = require('express')
+var bodyParser = require('body-parser')
+var app  = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
 
-
-var app  = express();
 const port = 3000
-
-app.use(express.static(__dirname));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
+	
+app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
 
 var messages = [
 	{name: "john", message: "hello"},
 	{name: "ceci", message: "hi"}
-];
+]
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/messages', (req, res) => {
-	res.send(messages);
-});
+	res.send(messages)
+})
 
 app.post('/messages', (req, res) => {
-	messages.push(req.body);
-	res.sendStatus(200);
-});
+	messages.push(req.body)
+	io.emit('message', req.body)
+	res.sendStatus(200)
+})
+io.on('connection', (socket) => {
+	console.log('user connected')
+})
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+var server = http.listen(port, () => {
+	console.log(`Example app listening on port ${port}!`)
+})
